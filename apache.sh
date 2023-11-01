7775681f9a3ac73abd827a2b1402a364dbf95fe7
goodman850 <virtualdesign@yahoo.com>
2023-11-01 17:12:51 +0330

    first commit

---
+++ apache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+confFile=app.conf
+#httpd_conf_file=/etc/apache2/ports.conf. #pi - debian
+httpd_conf=/etc/httpd/conf/httpd.conf
+newport=8080
+# Obtain from file if present:
+if [ -e $confFile ]; then
+  echo "checking configuration file..."
+  newport=$(grep server_port $confFile | awk -F= '{print $2}')
+fi
+echo "Switching to port $newport"
+
+if [ ! -e ${httpd_conf}.orig ]; then
+   sudo cp -p $httpd_conf ${httpd_conf}.orig
+fi
+sudo sed -i 's/Listen 80$/Listen 8080/' $httpd_conf
